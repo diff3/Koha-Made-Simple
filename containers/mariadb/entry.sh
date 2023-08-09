48c4da959e9cafa7276295787aa29b63735dd2b2
diff3 <magnus@entropy.nu>
2023-08-09 09:41:17 +0200

    fixed a bug in mariadb container

--- containers/mariadb/entry.sh
+++ containers/mariadb/entry.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-mysql -u $DB_ADMIN_USER -p$DB_ADMIN_PASS -e "CREATE DATABASE $DB_NAME"
-mysql -u $DB_ADMIN_USER -p$DB_ADMIN_PASS -e "CREATE USER '$DB_USER'@'%' IDENTIFIED BY '$DB_PASS';"
-mysql -u $DB_ADMIN_USER -p$DB_ADMIN_PASS -e "grant all privileges on $DB_NAME.* TO '$DB_USER'@'%' identified by '$DB_PASS'";
-mysql -u $DB_ADMIN_USER -p$DB_ADMIN_PASS -e "FLUSH PRIVILEGES";
+mariadb -u $DB_ADMIN_USER -p$DB_ADMIN_PASS -e "CREATE DATABASE $DB_NAME"
+mariadb -u $DB_ADMIN_USER -p$DB_ADMIN_PASS -e "CREATE USER '$DB_USER'@'%' IDENTIFIED BY '$DB_PASS';"
+mariadb -u $DB_ADMIN_USER -p$DB_ADMIN_PASS -e "grant all privileges on $DB_NAME.* TO '$DB_USER'@'%' identified by '$DB_PASS'";
+mariadb -u $DB_ADMIN_USER -p$DB_ADMIN_PASS -e "FLUSH PRIVILEGES";
